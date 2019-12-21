5736cbee705c5f69b438c11afe86b279caa1ecff
Jody <jodybamnz@gmail.com>
2019-12-21 17:40:03 +1300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Platformer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jodyb/Desktop/Platformer/Engine/Entities/BaseEntity.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/Entities/BaseEntity.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/Game.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/Game.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/GameStates/GameState.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/GameStates/GameState.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/GameStates/GameStateSingleplayer.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/GameStates/GameStateSingleplayer.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/InputManager.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/InputManager.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/Utility/Random.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/Utility/Random.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/Utility/Vector2F.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/Utility/Vector2F.cpp.obj"
+  "C:/Users/jodyb/Desktop/Platformer/Engine/main.cpp" "C:/Users/jodyb/Desktop/Platformer/cmake-build-debug/CMakeFiles/Platformer.dir/Engine/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files (x86)/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Platformer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Platformer.dir/Engine/Entities/BaseEntity.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/Game.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/GameStates/GameState.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/GameStates/GameStateSingleplayer.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/InputManager.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/Utility/Random.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/Utility/Vector2F.cpp.obj"
+  "CMakeFiles/Platformer.dir/Engine/main.cpp.obj"
+  "Platformer.exe"
+  "Platformer.exe.manifest"
+  "Platformer.pdb"
+  "libPlatformer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Platformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
